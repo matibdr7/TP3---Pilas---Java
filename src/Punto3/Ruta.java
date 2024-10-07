cd1835529d475c1f5f1e96e8d1755f452a88ad0b
@matibdr7 <matias.bdr7@gmail.com>
2024-10-07 12:09:06 -0300

    Implementacion de clase Ruta

---
+++ src/Punto3/Ruta.java
@@ -0,0 +1,22 @@
+package Punto3;
+
+public class Ruta {
+    private String origen;
+    private String destino;
+    private int distancia;
+
+    public Ruta() {
+    }
+
+    public Ruta(String origen, String destino, int distancia) {
+        this.origen = origen;
+        this.destino = destino;
+        this.distancia = distancia;
+    }
+
+    @Override
+    public String toString() {
+        return "Ruta [origen=" + origen + ", destino=" + destino + ", distancia=" + distancia + "]";
+    }
+
+}
