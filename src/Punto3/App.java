b6319181c5c6116a05a2b0b5c8b26734d3971270
@matibdr7 <matias.bdr7@gmail.com>
2024-10-07 12:48:10 -0300

    Modulo de menu funcionando

--- src/Punto3/App.java
+++ src/Punto3/App.java
@@ -6,9 +6,47 @@ de las rutas
  */
 
 package Punto3;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args){
-        
+        Scanner entrada = new Scanner(System.in);
+        GPS gps = new GPS();
+        menu(entrada);
+
+    }
+
+    public static void menu(Scanner entrada){
+        int decision;
+
+        while(true){
+            System.out.println(
+            "1- Agregar nueva ruta\n"+
+            "2- Deshacer ultima ruta \n"+
+            "3- Rehacer ruta deshecha \n"+
+            "4- Ver ruta actual\n"+
+            "5- Salir"
+        );
+
+            decision = Helper.getInteger("Ingrese el numero de la opcion que desea hacer: ", "Ingrese un valor valido");
+
+            switch (decision) {
+                case 1:
+                    System.out.println("1");
+                    break;
+                case 2:
+                    System.out.println("2");
+                    break;
+                case 3:
+                    System.out.println("3");
+                    break;
+                case 4:
+                    System.out.println("4");
+                    break;
+                case 5:
+                    System.out.println("Saliendo . . .");
+                    return;
+                }
+        }
     }
 }
