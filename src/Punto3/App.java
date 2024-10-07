fca55afc86c73d6c2370c2e025efc92f99e69458
@matibdr7 <matias.bdr7@gmail.com>
2024-10-07 12:06:16 -0300

    Base para el punto3

---
+++ src/Punto3/App.java
@@ -0,0 +1,14 @@
+/*
+Crear un programa para el sistema de manejo de rutas en un GPS utilizando una estructura de datos pila.
+Este sistema debe permitir a los usuarios agregar nuevas rutas, deshacer la última ruta agregada, rehacer
+una ruta deshecha y visualizar la ruta actual. También debe existir la opción para ver el historial completo
+de las rutas
+ */
+
+package Punto3;
+
+public class App {
+    public static void main(String[] args){
+        
+    }
+}
