a3e09818359a2e7cb1a2333e6d26f83812dcdd9d
@matibdr7 <matias.bdr7@gmail.com>
2024-10-07 00:48:27 -0300

    Base para punto2

---
+++ src/Punto2/App.java
@@ -0,0 +1,17 @@
+/*
+Escribir un programa que permita al usuario ingresar un número entero de cuatro cifras. Se debe validar
+que el número ingresado tenga exactamente cuatro cifras. Descomponer el número en dígitos y cargarlos
+en una pila. Luego, quitar los dígitos de la pila y formar un nuevo número de cuatro cifras (el número
+original al revés).
+Por ejemplo, si el usuario ingresa el número 5432, se debe descomponerlo y cargar cada dígito en la pila.
+Para este caso, se apilarían en el siguiente orden: 5, 4, 3 y 2. Al quitar los números de la pila se debe
+componer el número entero 2345. Además, mostrar los dígitos pares e impares del número original.
+ */
+
+package Punto2;
+
+public class App {
+    public static void main(String[] args){
+        
+    }
+}
