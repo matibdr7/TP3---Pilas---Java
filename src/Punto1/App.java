3705cb1258a1923eb329b2555a93b2216b2d9f70
@matibdr7 <matias.bdr7@gmail.com>
2024-10-06 22:16:16 -0300

    recorre vacia y llena las listas, no funciona pero no tira excepcion, falta corregir funcionamiento

--- src/Punto1/App.java
+++ src/Punto1/App.java
@@ -12,8 +12,59 @@ que no haya quedado vacía).
 
 package Punto1;
 
+import java.util.Random;
+import java.util.Scanner;
+
+
 public class App {
     public static void main(String[] args){
+
+        Random random = new Random();
+        Scanner entrada = new Scanner(System.in);
+
+        String[] palabras = {"Algoritmo", "Byte", "Cloud", "Hacker", "Internet", "Software", "Programacion", "CPU", "GPU", "LAN"};
         
+
+        Stack pilaAleatoria = pilaAleatoria(palabras, random);
+        System.out.println(pilaAleatoria);
+        Stack<String> pilaAuxiliar = new Stack<>();
+
+        /*solicitar al usuario que ingrese N palabras, las cuales deberán ser eliminadas de la pila cuando existan en ella y conservar las demás sin perder el orden inicial. */
+
+        pilaAleatoria = eliminarPalabras(entrada, pilaAleatoria, pilaAuxiliar);
+        System.out.println(pilaAleatoria);
+
+    }
+
+    public static Stack pilaAleatoria(String[] palabras, Random random){
+        Stack<String> pila = new Stack<>();
+        for (int i = 0; i <= 5; i++) {
+            pila.push(palabras[random.nextInt(10)]);
+        }
+        return pila;
+    }
+
+    public static Stack eliminarPalabras (Scanner entrada, Stack<String> pilaAleatoria, Stack<String> pilaAuxiliar){
+        System.out.println("¿Cuantas palabras desea eliminar?: ");
+        int n = entrada.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Ingrese la palabra a eliminar: ");
+            String palabra = entrada.nextLine();
+            entrada.next();
+
+            while(!pilaAleatoria.empty()){
+                String elemento = pilaAleatoria.pop();
+                if(!elemento.equals(palabra)){
+                    pilaAuxiliar.push(elemento);
+                }
+            }
+
+            while(!pilaAuxiliar.empty()){
+                pilaAleatoria.push(pilaAuxiliar.pop());
+            }
+
+        }
+        return pilaAleatoria;
     }
 }
