f5a1bc25264080dd5626528bdbee49c998b7bd52
@matibdr7 <matias.bdr7@gmail.com>
2024-10-08 16:12:00 -0300

    Base para el Punto5

---
+++ src/Punto5/App.java
@@ -0,0 +1,22 @@
+/*
+  Crear un programa que, mediante un menú, simule las operaciones de "deshacer" y "rehacer" en un editor
+de texto utilizando dos pilas, una para cada operación. Las acciones que se pueden realizar sobre un
+documento son: “negrita”, “cursiva”, “insertar tabla”, “subrayado”, “insertar imagen”, “escribir texto”,
+“borrar texto”, entre otras.
+
+Cada acción realizada sobre el documento se guarda en la pila “deshacer”. Generar de manera aleatoria un
+número de acciones a deshacer (ese número no puede superar el tamaño de la pila), lo cual implica
+remover dichas acciones de la pila “deshacer” y agregarlas a “rehacer”.
+Por otra parte, rehacer una o más acciones, a petición del usuario, en tanto no se supere el tamaño de la
+pila rehacer.
+
+Finalmente informar cuantas acciones relacionadas al formato se rehicieron.
+*/
+
+package Punto5;
+
+public class App {
+    public static void main(String[] args){
+
+    }
+}
