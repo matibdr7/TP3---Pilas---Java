ec0d16848f29af452a0c4bb40b2a4abbee00cf40
@matibdr7 <matias.bdr7@gmail.com>
2024-10-08 00:10:30 -0300

    Consigna del ejercicio

--- src/Punto4/App.java
+++ src/Punto4/App.java
@@ -1,7 +1,19 @@
+/*
+ * Implementar la clase “SitioWeb” que posea los siguientes atributos: nombre, tipo (educativo,
+entretenimiento, noticias o comercio electrónico) y url. Utilizar una pila para almacenar y recorrer el
+historial de navegación de los sitios web visitados.
+La operación de almacenar consiste en agregar sitios web a la pila, a petición del usuario. Recorrer el
+historial consiste en navegar hasta el sitio web de interés (por nombre o url), pero sin perder la información
+de los sitios previamente visitados (usar una pila auxiliar).
+Crear un programa que facilite el acceso a ambas operaciones. Además, mostrar el total de sitios web
+visitados e indicar cuantos corresponden a cada tipo.
+
+ */
+
 package Punto4;
 
 public class App {
     public static void main(String[] args){
-        
+
     }
 }
