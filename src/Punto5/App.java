22f8274a0d7118f34d86a7e6e4453635563b2b9e
@matibdr7 <matias.bdr7@gmail.com>
2024-10-08 16:59:59 -0300

    Modulo agregarAccion + Menu mejorado

--- src/Punto5/App.java
+++ src/Punto5/App.java
@@ -16,37 +16,56 @@ Finalmente informar cuantas acciones relacionadas al formato se rehicieron.
 
 package Punto5;
 import java.util.Scanner;
+import java.util.Random;
 
 public class App {
     public static void main(String[] args){
         Scanner entrada = new Scanner(System.in);
+        Random random = new Random();
 
         Stack deshacer = new Stack<>();
         Stack rehacer = new Stack<>();
 
-        menu(entrada, deshacer, rehacer);
+        menu(entrada, deshacer, rehacer, random);
     }
 
-    public static void menu(Scanner entrada, Stack deshacer, Stack reacher){
+    public static void menu(Scanner entrada, Stack deshacer, Stack rehacer, Random random){
         System.out.println(
             "---- MENU ----\n"+
-            "1- Rehacer\n"+
+            "1- Hacer algo\n"+
             "2- Deshacer\n"+
-            "3- Salir"
+            "3- Deshacer aleatoriamente\n"+
+            "4- Rehacer\n"+
+            "5- Salir"
         );
 
         int decision = Helper.getInteger("Ingrese lo que desee hacer: ", "Ingrese un valor numerico valido");
 
         switch (decision) {
             case 1:
-                break;
+                agregarAccion(entrada, rehacer);
             case 2:
                 break;
             case 3:
                 break;
+            case 4:
+                break;
+            case 5:
+                System.out.println("Saliendo . . .");
+                return;
             default:
                 System.out.println("Ingrese una opción valida");
-                menu(entrada, deshacer, reacher);
+                menu(entrada, deshacer, rehacer, random);
         }
     }
+
+    /*“negrita”, “cursiva”, “insertar tabla”, “subrayado”, “insertar imagen”, “escribir texto”, “borrar texto” */
+
+    public static void agregarAccion(Scanner entrada, Stack deshacer){
+        System.out.println("Ingrese lo que desea hacer: “negrita”, “cursiva”, “insertar tabla”, “subrayado”, “insertar imagen”, “escribir texto”, “borrar texto”, etc");
+
+        String accion = entrada.nextLine();
+        deshacer.push(accion);
+    }
+
 }
