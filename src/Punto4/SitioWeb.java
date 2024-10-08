d9c08715550c509a1a070765116ead6869b38472
@matibdr7 <matias.bdr7@gmail.com>
2024-10-08 12:36:37 -0300

    Setters y Getters para la clase SitioWeb

--- src/Punto4/SitioWeb.java
+++ src/Punto4/SitioWeb.java
@@ -14,6 +14,30 @@ public class SitioWeb {
         this.url = url;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
     
 
 }
