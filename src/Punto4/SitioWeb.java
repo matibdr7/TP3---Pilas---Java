3a2d52aaee2f596c355312d9760101333f304c40
@matibdr7 <matias.bdr7@gmail.com>
2024-10-08 00:20:02 -0300

    clase SitioWeb con constructores

---
+++ src/Punto4/SitioWeb.java
@@ -0,0 +1,19 @@
+package Punto4;
+
+public class SitioWeb {
+    private String nombre;
+    private String tipo;
+    private String url;
+
+    public SitioWeb() {
+    }
+
+    public SitioWeb(String nombre, String tipo, String url) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.url = url;
+    }
+
+    
+
+}
