7bae1fa1550f10bd34315b78de106b02e529955d
@matibdr7 <matias.bdr7@gmail.com>
2024-10-08 17:23:47 -0300

    Modulos de deshacer y rehacer funcionando

--- src/Punto5/App.java
+++ src/Punto5/App.java
@@ -30,36 +30,42 @@ public class App {
     }
 
     public static void menu(Scanner entrada, Stack deshacer, Stack rehacer, Random random){
-        System.out.println(
-            "---- MENU ----\n"+
-            "1- Hacer algo\n"+
-            "2- Deshacer\n"+
-            "3- Deshacer aleatoriamente\n"+
-            "4- Rehacer\n"+
-            "5- Salir"
-        );
-
-        int decision = Helper.getInteger("Ingrese lo que desee hacer: ", "Ingrese un valor numerico valido");
-
-        switch (decision) {
-            case 1:
-                agregarAccion(entrada, rehacer);
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                System.out.println("Saliendo . . .");
-                return;
-            default:
-                System.out.println("Ingrese una opción valida");
-                menu(entrada, deshacer, rehacer, random);
+        while(true){
+            System.out.println(
+                "---- MENU ----\n"+
+                "1- Hacer algo\n"+
+                "2- Deshacer\n"+
+                "3- Deshacer aleatoriamente\n"+
+                "4- Rehacer\n"+
+                "5- Mostrar pilas\n"+
+                "6- Salir"
+            );
+            
+            int decision = Helper.getInteger("Ingrese lo que desee hacer: ", "Ingrese un valor numerico valido");
+            
+            switch (decision) {
+                case 1:
+                    agregarAccion(entrada, deshacer);
+                    break;
+                case 2:
+                    deshacerAccion(entrada, deshacer, rehacer);
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    rehacerAccion(entrada, deshacer, rehacer);
+                case 5:
+                    System.out.println("Deshacer: "+deshacer +" Rehacer: "+ rehacer);
+                    break;
+                case 6:
+                    System.out.println("Saliendo . . .");
+                    return;
+                default:
+                    System.out.println("Ingrese una opción valida");
+                    menu(entrada, deshacer, rehacer, random);
+            }
         }
     }
-
-    /*“negrita”, “cursiva”, “insertar tabla”, “subrayado”, “insertar imagen”, “escribir texto”, “borrar texto” */
 
     public static void agregarAccion(Scanner entrada, Stack deshacer){
         System.out.println("Ingrese lo que desea hacer: “negrita”, “cursiva”, “insertar tabla”, “subrayado”, “insertar imagen”, “escribir texto”, “borrar texto”, etc");
@@ -68,4 +74,19 @@ public class App {
         deshacer.push(accion);
     }
 
+    public static void deshacerAccion(Scanner entrada, Stack deshacer, Stack rehacer){
+        if(!deshacer.empty()){
+            rehacer.push(deshacer.pop());
+        }else{
+            System.out.println("No hay acciones para deshacer . . .");
+        }
+    }
+
+    public static void rehacerAccion(Scanner entrada, Stack deshacer, Stack rehacer){
+        if(!rehacer.empty()){
+            deshacer.push(rehacer.pop());
+        }else{
+            System.out.println("No hay acciones para rehacer . . .");
+        }
+    }
 }
